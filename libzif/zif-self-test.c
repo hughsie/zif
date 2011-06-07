b9826dd288bc2c1b0a7ac333969c9fbd95001e52
Richard Hughes <richard@hughsie.com>
2011-06-07 09:28:10 +0100

    Revert "trivial: fix a random make check failure"
    This reverts commit b1b798d638f5d2abe377ab66498db75be99cefa0.

--- libzif/zif-self-test.c
+++ libzif/zif-self-test.c
@@ -1636,7 +1636,7 @@ zif_package_local_func (void)
 
 	/* fedora key */
 	g_assert_cmpstr (zif_package_local_get_key_id (ZIF_PACKAGE_LOCAL (pkg)), ==,
-			 "RSA/SHA256, Thu Sep 23 12:25:34 2010, Key ID 421caddb97a1071f");
+			 "RSA/SHA256, Thu Sep 23 17:25:34 2010, Key ID 421caddb97a1071f");
 
 	g_object_unref (pkg);
 
