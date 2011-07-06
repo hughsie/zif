ec19068c623d06ac83d01e5da7ee06808e450988
Richard Hughes <richard@hughsie.com>
2011-07-06 15:58:42 +0100

    trivial: add a bootstrap command to get all the BuildReqires installed

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+sudo yum install -y			\
+	bzip2-devel			\
+	docbook-utils			\
+	gettext				\
+	glib2-devel			\
+	gnome-doc-utils			\
+	gpgme-devel			\
+	gtk-doc				\
+	intltool			\
+	libarchive-devel		\
+	libsoup-devel			\
+	libtool				\
+	rpm-devel			\
+	sqlite-devel			\
+	zlib-devel
