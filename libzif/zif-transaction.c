798b4fc25cc76da86ce864789172fefbedd953ba
Richard Hughes <richard@hughsie.com>
2011-09-20 16:09:52 +0100

    trivial: fix RHEL6 compile

--- libzif/zif-transaction.c
+++ libzif/zif-transaction.c
@@ -4464,6 +4464,17 @@ out:
 	return ret;
 }
 
+#if !GLIB_CHECK_VERSION(2,28,0)
+#include <sys/time.h>
+static gint64
+g_get_real_time (void)
+{
+	struct timeval tv;
+	gettimeofday (&tv, NULL);
+	return (tv.tv_sec * G_USEC_PER_SEC) + tv.tv_usec;
+}
+#endif
+
 /**
  * zif_transaction_write_history:
  **/
